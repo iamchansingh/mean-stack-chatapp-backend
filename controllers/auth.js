968a41c76f0cf0dad9214f07820b566f86d0367c
chan <chanpreet.shampy@gmail.com>
2018-11-25 14:51:44 +0530

    few server related changes and cors added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,55 @@
-const joi=require('joi');
-module.exports={
-    CreateUser(req,res){
-        const schema=joi.object().keys({
-            username:joi.string().min(5).max(10).required(),
-            email:joi.string().email().required(),
-            password:joi.string().min(5).required()
+const joi = require('joi');
+const HttpStatus = require('http-status-codes');
+const user = require('../models/userModels')
+const helpers = require('../helpers/helpers');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const dbconfig = require('../config/secrets')
+module.exports = {
+    async CreateUser(req, res) {
+        const schema = joi.object().keys({
+            username: joi.string().min(5).max(10).required(),
+            email: joi.string().email().required(),
+            password: joi.string().min(5).required()
         })
 
-        const {error,value}=joi.validate(req.body,schema);
-        if(error &&error.details){
-            return res.status(500).json({message:error.details});
+        const { error, value } = joi.validate(req.body, schema);
+        console.log(value);
+        if (error && error.details) {
+            return res.status(HttpStatus.BAD_REQUEST).json({ msg: error.details });
 
-        }        
+        }
+        const userEmail = await user.findOne({ email: helpers.lowerCase(req.body.email) });
+        if (userEmail) {
+            return res.status(HttpStatus.CONFLICT).json({ message: 'Email already exists' })
+
+        }
+
+        const userName = await user.findOne({ username: helpers.lowerCase(req.body.username) });
+        if (userName) {
+            return res.status(HttpStatus.CONFLICT).json({ message: 'username already exists' })
+
+        }
+        console.log("in here 1")
+        return bcrypt.hash(value.password, 10, (err, hash) => {
+            console.log("in here 2")
+            if (err) {
+                return res.status(HttpStatus.BAD_REQUEST).json({ message: 'error hashing password' });
+            }
+            const body = {
+                username: helpers.firstUpper(value.username),
+                email: helpers.lowerCase(value.email),
+                password: hash
+            }
+            user.create(body).then((user) => {
+                const token = jwt.sign({data:user}, dbconfig.secret, {
+                    expiresIn: 120
+                });
+                res.cookie('auth',token);
+                res.status(HttpStatus.CREATED).json({ message: 'user created succesfully' ,user,token})
+            }).catch(err => {
+                res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'error occured' })
+            })
+        });
     }
 } 
--- server.js
+++ server.js
@@ -1,17 +1,51 @@
-const express = require('express');
-const mongoose= require ('mongoose');
-const dbConfig=require('./config/secrets')
-const app=express();
-const cookieParser=require('cookie-parser');
-const logger =require('morgan');
+const express = require("express");
+const mongoose = require("mongoose");
+const dbConfig = require("./config/secrets");
+var fs = require('fs')
+var https = require('https')
+const app = express();
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET",
+    "POST",
+    "DELETE",
+    "PUT",
+    "OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,x-Requested-With, Content-Type,Accept,Authorization"
+  );
+  next();
+});
 app.use(cookieParser());
-app.use(logger('dev'));
-app.use(express.json({limit:'50mb'}));
-app.use(express.urlencoded({extended:true,limit:'50mb' }))
-mongoose.Promise=global.Promise;
-mongoose.connect(dbConfig.url, { useNewUrlParser: true });
-const auth=require('./routes/authRoutes');
-app.use('/api/chatapp',auth);
-app.listen(3000,()=>{
-    console.log('running babes');
-})
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  dbConfig.url,
+  { useNewUrlParser: true }
+);
+const auth = require("./routes/authRoutes");
+app.use("/api/chatapp", auth);
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert")
+    },
+    app
+  )
+  .listen(3000, () => {
+    console.log("running babes");
+  });
